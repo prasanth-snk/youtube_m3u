56c737fc495532a4eacc14b1a530fa1032a888a3
prasanthsnk <prasanth.snk@gmail.com>
2023-03-02 14:38:45 +0530

    changes

--- scripts/JTV_RefreshToken.py
+++ scripts/JTV_RefreshToken.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 
 import requests
+import json
 import os
 import sys
 
@@ -8,18 +9,23 @@ windows = False
 if 'win' in sys.platform:
     windows = True
 
+url = "https://auth.media.jio.com/tokenservice/apis/v1/refreshtoken"
 
-sUrl = "https://auth.media.jio.com/tokenservice/apis/v1/refreshtoken"
-body = {
-"refreshToken":"c1df31fd-4866-4c02-bd8e-508dc9df3696",
-"devicetype" : "phone",
-"versionCode" : 290,
-"os" : "android",
-"appName": "RJIL_JioTV",
-"deviceId" : "3c6d6b5702fa09bd"
+payload = json.dumps({
+  "refreshToken": "c1df31fd-4866-4c02-bd8e-508dc9df3696",
+  "devicetype": "phone",
+  "versionCode": 290,
+  "os": "android",
+  "appName": "RJIL_JioTV",
+  "deviceId": "3c6d6b5702fa09bd"
+})
+headers = {
+  'Content-Type': 'application/json'
 }
-r = requests.post(url = sUrl, data = body).text
-print(f"{r}")
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
 
 if 'temp.txt' in os.listdir():
     os.system('rm temp.txt')
