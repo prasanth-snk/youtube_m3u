c32e6a3cf047672bdd5a0f62db962306483a781f
prasanthsnk <prasanth.snk@gmail.com>
2023-03-02 14:34:18 +0530

    changes

--- scripts/JTV_RefreshToken.py
+++ scripts/JTV_RefreshToken.py
@@ -1,18 +1,26 @@
 #! /usr/bin/python3
 
 import requests
+import os
+import sys
+
+windows = False
+if 'win' in sys.platform:
+    windows = True
 
 
+sUrl = "https://auth.media.jio.com/tokenservice/apis/v1/refreshtoken"
+body = {
+"refreshToken":"c1df31fd-4866-4c02-bd8e-508dc9df3696",
+"devicetype" : "phone",
+"versionCode" : 290,
+"os" : "android",
+"appName": "RJIL_JioTV",
+"deviceId" : "3c6d6b5702fa09bd"
+}
+r = requests.post(url = sUrl, data = body).text
+print(f"{r}")
 
-    sUrl = "https://auth.media.jio.com/tokenservice/apis/v1/refreshtoken"
-    body = {
-    "refreshToken":"c1df31fd-4866-4c02-bd8e-508dc9df3696",
-    "devicetype" : "phone",
-    "versionCode" : 290,
-    "os" : "android",
-    "appName": "RJIL_JioTV",
-    "deviceId" : "3c6d6b5702fa09bd"
-    }
-    r = requests.post(url = sUrl, data = body).text
-    print(f'"{r}"')
-
+if 'temp.txt' in os.listdir():
+    os.system('rm temp.txt')
+    os.system('rm watch*')
